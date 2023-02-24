bdb62130826cf0238f8d71092ebcaa6a85c190df
admin <daniiidani883@gmail.com>
2023-02-24 13:23:51 +0700

    mengubah halaman login

--- sertifikat/src/views/HomeView.vue
+++ sertifikat/src/views/HomeView.vue
@@ -3,9 +3,8 @@
     <div class="container-fluid h-custom">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-md-9 col-lg-6 col-xl-5">
-
         </div>
-        <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+          <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
           <form>
             <div class="right">
               <!-- Email input -->
@@ -17,7 +16,6 @@
                   placeholder="Username"
                 />
               </div>
-
               <!-- Password input -->
               <div class="form-outline mb-3">
                 <input
@@ -29,32 +27,18 @@
               </div>
               <div class="text-center text-lg-start mt-4 pt-2">
                 <router-link to="/kontak" class="btn btn-primary btn-lg" style="padding-left: 2.5rem; padding-right: 2.5rem"> Login </router-link>
-                <p class="small fw-bold mt-2 pt-1 mb-0">
-                  Don't have an account?
-                <router-link to="/register" class="link-danger"> Register </router-link>
+                <!-- <p class="small fw-bold mt-2 pt-1 mb-0">
+                  Forget
+                </p> -->
+                <p class="small fw-bold mt-2 pt-1 mb-0 text-primary fw-bold"> Forget 
+                  <router-link to="/register" style="text-decoration: none;"> Password? </router-link>
                 </p>
               </div>
             </div>
           </form>
         </div>
+        </div>
       </div>
-    </div>
-      <!-- Right -->
-      <div>
-        <a href="#!" class="text-white me-4">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="#!" class="text-white me-4">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="#!" class="text-white me-4">
-          <i class="fab fa-google"></i>
-        </a>
-        <a href="#!" class="text-white">
-          <i class="fab fa-linkedin-in"></i>
-        </a>
-      </div>
-      <!-- Right -->
   </section>
 </template>
 
